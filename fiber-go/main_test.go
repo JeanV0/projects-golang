1f4dffc00a05b66be3e9778dc11d4c8b67d86722
JeanVictor0 <jean.roque@pigz.com.br>
2023-01-24 18:01:43 -0400

    feat: docker ( in test not working ) and structure

--- fiber-go/main_test.go
+++ fiber-go/main_test.go
@@ -1,9 +1,7 @@
 package main
 
 import (
-	"marketplace/Tests/controller/api"
 	"marketplace/Tests/middleware"
-	repository "marketplace/Tests/repository"
 	dotenv "marketplace/config/services/Dotenv"
 	db "marketplace/config/services/db"
 	"marketplace/src"
@@ -34,6 +32,4 @@ func getApp() *fiber.App {
 	return src.App
 }
 
-func TestMiddleware(t *testing.T)    { app := getApp(); middleware.TestMiddleware(app, t) }
-func TestApiHightlight(t *testing.T) { app := getApp(); api.TestApiHightlight(app, t) }
-func TestCityDatabase(t *testing.T)  { getDb(); repository.TestCityDatabase(t) }
+func TestMiddleware(t *testing.T) { app := getApp(); middleware.TestMiddleware(app, t) }
